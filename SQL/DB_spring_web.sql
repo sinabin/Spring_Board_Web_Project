07669a5139938b1fb07e6cd6a41caab30f8a62a4
DESKTOP-JOLA0AF\see <sinabin@naver.com>
2023-09-21 19:20:46 +0900

    테이블 생성 순서 변경

--- SQL/DB_spring_web.sql
+++ SQL/DB_spring_web.sql
@@ -1,18 +1,30 @@
 CREATE DATABASE IF NOT EXISTS `spring_web` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `spring_web`;
 
+-- 테이블 spring_web.tbl_user 구조 내보내기
+CREATE TABLE IF NOT EXISTS `tbl_user` (
+    `user_id` varchar(50) NOT NULL,
+    `user_pw` varchar(50) NOT NULL,
+    `user_nick` varchar(50) NOT NULL,
+    `user_email` varchar(50) NOT NULL,
+    `user_reg_date` timestamp NULL DEFAULT curdate(),
+    `user_grade` varchar(50) DEFAULT '일반계정',
+    PRIMARY KEY (`user_id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=UTF8MB4_GENERAL_CI;
+
+
 CREATE TABLE IF NOT EXISTS `tbl_board` (
-  `bno` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) NOT NULL DEFAULT '',
-  `content` text DEFAULT '',
-  `user_id` varchar(50) NOT NULL,
-  `category` varchar(50) NOT NULL DEFAULT '자유게시판',
-  `board_reg_date` timestamp NULL DEFAULT current_timestamp(),
-  `view_cnt` int(11) DEFAULT 0,
-  PRIMARY KEY (`bno`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `tbl_board_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+    `bno` int(11) NOT NULL AUTO_INCREMENT,
+    `title` varchar(100) NOT NULL DEFAULT '',
+    `content` text DEFAULT '',
+    `user_id` varchar(50) NOT NULL,
+    `category` varchar(50) NOT NULL DEFAULT '자유게시판',
+    `board_reg_date` timestamp NULL DEFAULT current_timestamp(),
+    `view_cnt` int(11) DEFAULT 0,
+    PRIMARY KEY (`bno`),
+    KEY `user_id` (`user_id`),
+    CONSTRAINT `tbl_board_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 INSERT IGNORE INTO `tbl_board` (`bno`, `title`, `content`, `user_id`, `category`, `board_reg_date`, `view_cnt`) VALUES
@@ -21,16 +33,6 @@ INSERT IGNORE INTO `tbl_board` (`bno`, `title`, `content`, `user_id`, `category`
 	(3, '제품 Q&A 게시판', '<p style="user-select: auto;">제품 Q&amp;A 게시판입니다.<br style="user-select: auto;"></p>', 'adminTester', '제품 Q&A', '2022-02-20 01:37:24', 2),
 	(4, '개인정보보호 및 관리 토론 게시판', '<p style="user-select: auto;">개인정보보호 및 관리 토론 게시판입니다.<br style="user-select: auto;"></p>', 'adminTester', '개인정보보호 및 관리 토론', '2022-02-20 01:37:40', 4);
 
--- 테이블 spring_web.tbl_user 구조 내보내기
-CREATE TABLE IF NOT EXISTS `tbl_user` (
-  `user_id` varchar(50) NOT NULL,
-  `user_pw` varchar(50) NOT NULL,
-  `user_nick` varchar(50) NOT NULL,
-  `user_email` varchar(50) NOT NULL,
-  `user_reg_date` timestamp NULL DEFAULT curdate(),
-  `user_grade` varchar(50) DEFAULT '일반계정',
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 INSERT IGNORE INTO `tbl_user` (`user_id`, `user_pw`, `user_nick`, `user_email`, `user_reg_date`, `user_grade`) VALUES
